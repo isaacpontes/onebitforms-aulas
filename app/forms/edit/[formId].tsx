248e34632211549df738cfe67e18541913593c31
Isaac Pontes <isaaclfp@outlook.com>
2025-06-11 23:32:44 -0300

    Aula 10: Atualizando o formulário no Supabase

--- app/forms/edit/[formId].tsx
+++ app/forms/edit/[formId].tsx
@@ -3,10 +3,11 @@ import { Input } from "@/components/ui/Input";
 import { ScreenContainer } from "@/components/ui/ScreenContainer";
 import { Switch } from "@/components/ui/Switch";
 import { Title } from "@/components/ui/Title";
+import formsService from "@/services/forms-service";
 import { Theme, useTheme } from "@/themes/ThemeContext";
 import { useLocalSearchParams } from "expo-router";
-import { useState } from "react";
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function EditFormScreen() {
   const { formId } = useLocalSearchParams();
@@ -16,6 +17,25 @@ export default function EditFormScreen() {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [isPublished, setIsPublished] = useState(false);
+
+  useEffect(() => {
+    if (typeof formId !== 'string') {
+      Alert.alert('Error', 'Invalid form id.');
+      return;
+    }
+    formsService.getFormById(formId).then((data) => {
+      if (!data) return;
+      setTitle(data.form.title);
+      setDescription(data.form.description ?? '');
+      setIsPublished(data.form.isPublished);
+    });
+  }, []);
+
+  const saveForm = async () => {
+    if (typeof formId !== 'string') return;
+    await formsService.updateForm(formId, { title, description, isPublished });
+    Alert.alert('Success', 'Form saved successfuly!');
+  }
 
   return (
     <ScreenContainer>
@@ -31,7 +51,7 @@ export default function EditFormScreen() {
           <Switch label="Published?" value={isPublished} onValueChange={setIsPublished} />
         </View>
 
-        <Button title="Save form" onPress={() => { }} />
+        <Button title="Save form" onPress={() => saveForm()} />
 
         <View style={styles.buttonsRow}>
           <Button title="Preview form" style={{ flex: 1 }} variant="outline" onPress={() => { }} />
